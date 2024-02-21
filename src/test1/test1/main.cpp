960e670d5b5772339219462341aa1b9db31b3c11
imc <i_cenov@botevgrad.com>
2024-02-21 09:17:44 +0200

    USE_HEX_DUMP: Removed usage of printf, added pointer to output function

--- src/test1/test1/main.cpp
+++ src/test1/test1/main.cpp
@@ -1979,7 +1979,7 @@ int test_23_32(void)
 
 #if defined(USE_HEX_DUMP)
 
-char test_string[] = "This is a test string. It will be dumped on stdout. А това е на български език.";
+char test_string[] = "This is a test string. It will be dumped on stdout.";
 
 #define HEXD_BYTE       (0x01u)
 #define HEXD_WORD       (0x02u)
@@ -1987,30 +1987,74 @@ char test_string[] = "This is a test string. It will be dumped on stdout. А т�
 #define HEXD_LSBFIRST   (0x00u)
 #define HEXD_MSBFIRST   (0x80u)
 
-void DumpHex(const void* data, size_t size) {
+#define HEXD_BUFLEN     (256)
+
+static uint16_t hex_to_asc(uint8_t v)
+{
+    uint8_t t;
+    uint16_t r;
+
+    t = v & 0x0fu;
+    ((uint8_t* )&r)[1] = (t > 9u) ? (t + ('A' - 10u)) : (t + '0');   // LS byte
+    t = v >> 4;
+    ((uint8_t* )&r)[0] = (t > 9u) ? (t + ('A' - 10u)) : (t + '0');   // MS byte
+
+    return r;
+}
+
+static void append_ascii(char* buf, const char* tail)
+{
+    strcat(buf,"|  ");
+    strcat(buf,tail);
+    strcat(buf," \n");
+}
+
+static void printf_line(const char* line)
+{
+    printf("%s",line);
+}
+
+typedef void (*output_line)(const char* line);
+
+// this buffer is big enough to contain full line of 16 number and their ASCII representation
+static char strBuffer[HEXD_BUFLEN];
+
+void dump_hex(const void* data, size_t size, output_line func) {
     char ascii[17];
-    size_t i, j;
+    size_t i, j, sx;
     ascii[16] = '\0';
+    sx = 0;
+    memset(strBuffer,'\0',sizeof(strBuffer));
     for (i = 0; i < size; ++i) {
-        printf("%02X ", ((unsigned char*)data)[i]);
+        uint16_t hdig = hex_to_asc(((unsigned char*)data)[i]);
+        strBuffer[sx++] = ((uint8_t* )&hdig)[0];
+        strBuffer[sx++] = ((uint8_t* )&hdig)[1];
+        strBuffer[sx++] = ' ';
+
         if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= 255) { // '~'
             ascii[i % 16] = ((unsigned char*)data)[i];
         } else {
             ascii[i % 16] = '.';
         }
         if ((i+1) % 8 == 0 || i+1 == size) {
-            printf(" ");
+            strBuffer[sx++] = ' ';
             if ((i+1) % 16 == 0) {
-                printf("|  %s \n", ascii);
+                goto output;
             } else if (i+1 == size) {
                 ascii[(i+1) % 16] = '\0';
                 if ((i+1) % 16 <= 8) {
-                    printf(" ");
+                    strBuffer[sx++] = ' ';
                 }
                 for (j = (i+1) % 16; j < 16; ++j) {
-                    printf("   ");
+                    strBuffer[sx++] = ' ';
+                    strBuffer[sx++] = ' ';
+                    strBuffer[sx++] = ' ';
                 }
-                printf("|  %s \n", ascii);
+output:
+                append_ascii(strBuffer,ascii);
+                func(strBuffer);
+                memset(strBuffer,'\0',sizeof(strBuffer));
+                sx = 0;
             }
         }
     }
@@ -2018,7 +2062,7 @@ void DumpHex(const void* data, size_t size) {
 
 int test_hexdump(void)
 {
-    DumpHex(test_string,sizeof(test_string));
+    dump_hex(test_string,sizeof(test_string),printf_line);
 
     return 0;
 }
